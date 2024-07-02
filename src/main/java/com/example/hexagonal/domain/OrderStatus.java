9cb2e31d11324e46dc7398981c9c50142a4a78a8
Raul Lopez <rlop666@gmail.com>
2024-07-02 14:38:43 +0200

    Día 2

---
+++ src/main/java/com/example/hexagonal/domain/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.example.hexagonal.domain;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    SHIPPED,
+    DELIVERED,
+    CANVELLED
+}
--- src/test/java/com/example/hexagonal/application/ProductService.java
+++
@@ -1,7 +0,0 @@
-package com.example.hexagonal.application;
-
-public class ProductService {
-    public String getProducts(){
-        return "Listado de productos";
-    }
-}
